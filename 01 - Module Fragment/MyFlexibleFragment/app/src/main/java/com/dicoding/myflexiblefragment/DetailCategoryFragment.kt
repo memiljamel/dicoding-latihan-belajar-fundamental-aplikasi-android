46d6b296a7ea2f2762b93c255542333d86e31a6d
Emil Jamel Mahmuda <memiljamel@gmail.com>
2023-06-07 12:21:59 +0700

    Codelab Memanggil Activity dari Fragment

--- 01 - Module Fragment/MyFlexibleFragment/app/src/main/java/com/dicoding/myflexiblefragment/DetailCategoryFragment.kt
+++ 01 - Module Fragment/MyFlexibleFragment/app/src/main/java/com/dicoding/myflexiblefragment/DetailCategoryFragment.kt
@@ -1,5 +1,6 @@
 package com.dicoding.myflexiblefragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -48,6 +49,11 @@ class DetailCategoryFragment : Fragment() {
             val optionDialogFragment = OptionDialogFragment()
             val fragmentManager = childFragmentManager
             optionDialogFragment.show(fragmentManager, OptionDialogFragment::class.java.simpleName)
+        }
+
+        btnProfile.setOnClickListener {
+            val intent = Intent(requireActivity(), ProfileActivity::class.java)
+            startActivity(intent)
         }
     }
 
