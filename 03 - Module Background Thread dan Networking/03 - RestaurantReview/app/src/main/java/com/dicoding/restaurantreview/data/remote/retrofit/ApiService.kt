65226ff7ac54092db833b3d28bf126d1654f8737
Emil Jamel Mahmuda <memiljamel@gmail.com>
2023-06-16 21:52:31 +0700

    Latihan Networking dengan Retrofit

---
+++ 03 - Module Background Thread dan Networking/03 - RestaurantReview/app/src/main/java/com/dicoding/restaurantreview/data/remote/retrofit/ApiService.kt
@@ -0,0 +1,23 @@
+package com.dicoding.restaurantreview.data.remote.retrofit
+
+import com.dicoding.restaurantreview.data.remote.response.PostReviewResponse
+import com.dicoding.restaurantreview.data.remote.response.RestaurantResponse
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+
+    @GET("detail/{id}")
+    fun getRestaurant(
+        @Path("id") id: String,
+    ): Call<RestaurantResponse>
+
+    @FormUrlEncoded
+    @Headers("Authorization: token 12345")
+    @POST("review")
+    fun postReview(
+        @Field("id") id: String,
+        @Field("name") name: String,
+        @Field("review") review: String,
+    ): Call<PostReviewResponse>
+}
